e305f656e576b957d90ebc1af2ccf668b3a87ce1
slvn <sylvain.dumaine@hotmail.fr>
2023-07-12 16:55:33 +0200

    add some js

---
+++ assets/js/lib/playlist_hiphop.js
@@ -0,0 +1,15 @@
+const playlist_hiphop = [
+    {
+        "title" : "jkdslfjlksdjgf",
+        "author" : "sqkfljlkfj",
+        "cover" : "./assets/img/cover/macover.jpg",
+        "year" : 1994,
+        "audio" : "./assets/audio/blabla.mp3",
+        "genre" : ['hiphop',"punk","jazz"],
+        "description" : "lorem"
+
+    },
+    //demain il faudra 2 tracks de plus
+]
+
+export {playlist_hiphop}
---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+// lorsque j'importe un ou plusieurs élémentHTML je le fais yjs en début de script
+import { playlist_hiphop } from "./lib/playlist_hiphop.js";
+console.dir(playlist_hiphop); 
+console.log("Hello");
